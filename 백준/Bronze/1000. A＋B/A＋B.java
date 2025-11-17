3e36afee4c7f3fa81a495b024fe1409b61157ec1
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2025-11-17 12:49:51 +0900

    [Bronze V] Title: A+B, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        String[] arr = input.split(" "); 
+        int first = Integer.parseInt(arr[0]);
+        int second = Integer.parseInt(arr[1]);
+        System.out.println(first + second);
+    }
+}
