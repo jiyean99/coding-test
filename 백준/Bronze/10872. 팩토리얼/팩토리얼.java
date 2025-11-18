6aa67d72dd38459bc842af337b44f1638f5eee4c
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2025-11-18 18:27:41 +0900

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        int n = Integer.parseInt(input);
+
+        int result = 1;
+        for (int i = 2; i <= n; i++){
+            result = result * i;
+        }
+
+        System.out.println(result);
+    }
+}
