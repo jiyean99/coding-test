de41401973da5557ab9c890929c71a168a458289
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2025-11-30 18:19:17 +0900

    [Bronze III] Title: 최소, 최대, Time: 468 ms, Memory: 92492 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        arr[0] = Integer.parseInt(st.nextToken());
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println(min + " " + max);
+    }
+}
