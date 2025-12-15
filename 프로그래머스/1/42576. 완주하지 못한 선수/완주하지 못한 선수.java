cee5c3be025612dbdb9f341c6428a41ccc683c8e
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2025-12-15 18:18:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.77 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,31 +1,18 @@
-import java.io.*;
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-       Map<String, Integer> my_map = new HashMap<>();
-
+        Map<String,Integer> myMap = new HashMap<>();
         String answer = "";
-
-        for (String a : participant) {
-            if (my_map.containsKey(a)) {
-                my_map.put(a, my_map.get(a) + 1);
-            } else {
-                my_map.put(a, 1);
-            }
-
+        for(String s : participant){
+            myMap.put(s,myMap.getOrDefault(s,0)+1);
         }
-        for (String a : completion) {
-            if (my_map.containsKey(a)){
-                if (my_map.get(a) == 1) {
-                    my_map.remove(a);
-                } else {
-                    my_map.put(a, my_map.get(a) - 1);
-                }
-            }
+        for(String s : completion){
+            myMap.put(s,myMap.getOrDefault(s,0)-1);
         }
-        for (String a : my_map.keySet()) {
-            answer = a;
+        for(String s: myMap.keySet()){
+            if(myMap.get(s)>0){
+                return s;
+            }
         }
         return answer;
     }
