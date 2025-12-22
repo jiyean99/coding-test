88ca5973530e89cf5bef4108e588914554a2e533
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2025-12-22 13:09:33 +0900

    [Bronze II] Title: 피보나치 수, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//피보나치 수
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[46];
+        dp[1] = dp[2] = 1;
+        for (int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
+}
