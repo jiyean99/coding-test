4c58fed2a14583e23eaeb8506352651c22d5dc0a
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2025-12-30 15:47:57 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -9,7 +9,7 @@ class Solution {
         arr[2] = 2;
         
         for(int i = 3; i <= n; i++){
-            arr[i] = (arr[i-1] + arr[i-2])% 1234567;
+            arr[i] = (arr[i-1] % 1234567 + arr[i-2] % 1234567) % 1234567;
         }
         long answer = arr[n] ;
         return answer;
