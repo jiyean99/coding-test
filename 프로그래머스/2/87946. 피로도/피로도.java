d7474b7963714d9044734a6fab3bde28751f9340
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2025-12-31 05:38:43 +0900

    [level 2] Title: 피로도, Time: 3.70 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,29 @@
+class Solution {
+    static int answer = 0;
+    static boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        // answer: 최대 던전 수
+        // k: 현재 피로도
+        // dungeons: {최소필요 피로도, 소모 피로도}
+        // 최소필요 피로도와 현재 피로도를 비교 후 현재 피로도가 더 크면 소모피로도를 차감 후 던전 수를 ++ 한다.
+        // 이 때 이차원 배열의 던전을 돌 수 있는 방법을 순열로 구하면 됨
+        visited = new boolean[dungeons.length];
+        backtrack(k, dungeons, 0);
+        
+        return answer;
+    }
+    
+    static void backtrack(int k, int[][] dungeons, int count) {
+        answer = Math.max(answer, count);
+
+        for (int i = 0; i < dungeons.length; i++) {
+            if (visited[i]) continue;
+            if (k >= dungeons[i][0]) {
+                visited[i] = true;
+                backtrack(k - dungeons[i][1], dungeons, count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
