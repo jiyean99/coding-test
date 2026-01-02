8f90e8862ea406dbab2f1db234e20f409ad4b0cb
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2026-01-02 12:45:48 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 59.46 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int startIdx = 0;
+        int endIdx = 0;
+        int sum = sequence[startIdx];
+
+        List<int[]> list = new ArrayList<>();
+
+        while (startIdx <= endIdx && endIdx < sequence.length) {
+            if (sum == k) {
+                list.add(new int[]{startIdx, endIdx});
+                sum -= sequence[startIdx];
+                startIdx++;
+            } else if (sum > k) { // 수열의 합이 k보다 클 때
+                sum -= sequence[startIdx]; // 우선 시작인덱스의 값만큼 빼주고
+                startIdx++; // 시작인덱스를 늘려준다
+                if (startIdx > endIdx) { // 시작인덱스가 끝인덱스를 역전해버릴 경우
+                    endIdx++; // 일단 끝인덱스를 더함
+                    if (sequence.length == endIdx) break; // 당연히 끝인덱스가 길이 초과하면 종료
+                    sum += sequence[endIdx];
+                }
+            } else if (sum < k) { // 수열의 합이 k보다 작을 때
+                endIdx++; // 우선 끝인덱스를 늘려주고
+                if (sequence.length == endIdx) break;// 이 때 끝인덱스가 길이 초과하면 종료
+                sum += sequence[endIdx]; // 늘려준값을 추가해준다.
+            }
+        }
+
+        int[] answer = new int[2];
+        int arrMinLength = Integer.MAX_VALUE;
+
+        for (int[] a : list) {
+            int arrLength = a[1] - a[0] + 1;
+            if (arrMinLength > arrLength) {
+                arrMinLength = arrLength;
+                answer = a;
+            }
+        }
+        
+        return answer;
+    }
+}
