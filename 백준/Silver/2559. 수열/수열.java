3d0299e47f54947a47e255d2eb1ba147e96e1404
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2026-01-03 18:39:11 +0900

    [Silver III] Title: 수열, Time: 256 ms, Memory: 23904 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 측정한 날짜 수
+        int k = Integer.parseInt(st.nextToken()); // 연속적인 날짜 수
+
+        int[] dayTemp = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            dayTemp[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for (int i = 0; i < k; i++) {
+            currentSum += dayTemp[i];
+        }
+        maxSum = currentSum;
+
+        for (int i = 1; i <= n - k; i++) {
+            currentSum += dayTemp[i + k - 1] - dayTemp[i - 1];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        System.out.println(maxSum);
+
+    }
+}
