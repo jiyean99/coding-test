30e491064432e8e664babacdedad9b5e07a4a555
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2026-01-19 19:08:56 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int[] cnt = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i) - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            sb.append(cnt[i]);
+            if (i < 25) sb.append(' ');
+        }
+        System.out.print(sb.toString());
+    }
+}
