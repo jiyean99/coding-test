6c6b587a5eabae20b563a94e051f13c6e4fc4dbd
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2026-03-22 19:05:37 +0900

    [Bronze IV] Title: 2023은 무엇이 특별할까?, Time: 468 ms, Memory: 28324 KB -BaekjoonHub

---
+++ 백준/Bronze/31090. 2023은 무엇이 특별할까？/2023은 무엇이 특별할까？.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+
+            int lastTwo = N % 100;
+
+            if ((N + 1) % lastTwo == 0) {
+                System.out.println("Good");
+            } else {
+                System.out.println("Bye");
+            }
+        }
+    }
+}
